2ad33bef6b4f7015c70c5ae9a57d55c5b297a3b2
umezaki <bungoume3@yahoo.co.jp>
2015-12-23 11:05:26 +0900

    Add main test

--- test/plugin/test_filter_geoip.rb
+++ test/plugin/test_filter_geoip.rb
@@ -23,6 +23,19 @@ class GeoipFilterTest < Test::Unit::TestCase
 
   def test_emit
     d1 = create_driver(CONFIG)
+    ip_iddr = '93.184.216.34'
+
+    d1.run do
+      d1.emit({'client_ip' => ip_iddr})
+    end
+    emits = d1.emits
+    assert_equal 1, emits.length
+    assert_equal 'test', emits[0][0] # tag
+    geo_object = emits[0][2]['geo']
+    assert_equal [-70.8228, 42.150800000000004], geo_object['coordinates']
+    assert_equal 'US', geo_object['country_code']
+    assert_equal 'Norwell', geo_object['city']
+    assert_equal 'MA', geo_object['region_code']
   end
 
 end
